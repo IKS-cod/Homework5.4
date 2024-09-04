15104284b5bf942c1e526b3c8505a014e2489613
IKS-cod <ik.smirnov@yandex.ru>
2024-09-04 21:23:29 +0300

    ответ на билет 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Билет 3");
+//Напишите код, с помощью которого можно разделить строку на части.
+//
+//Код должен работать для строк любого размера и любого содержания. Размер частей вы устанавливаете самостоятельно.
+        String string ="Я, делаю домашнюю работу";
+        String stringNew = string.trim();
+        String[]words=stringNew.split(" ");
+        System.out.println(Arrays.toString(words));
+
+
+
+
     }
 }
